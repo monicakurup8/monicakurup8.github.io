dc250dd31546f29088aeae6b56a5abcb2b4a25b1
Aswin Nair <aswintorch@gmail.com>
2019-10-22 19:30:49 -0400

    Updates

---
+++ precache-manifest.98eb881f36bc6640f4beb467d127ad34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd8622b39065b1b8667f042e3fcfc5a4",
+    "url": "/monicakurup8/monicakurup8.github.io/index.html"
+  },
+  {
+    "revision": "9016b11b92bf0be53e2a",
+    "url": "/monicakurup8/monicakurup8.github.io/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "acae69c873e509ca92e6",
+    "url": "/monicakurup8/monicakurup8.github.io/static/js/2.070f0749.chunk.js"
+  },
+  {
+    "revision": "9016b11b92bf0be53e2a",
+    "url": "/monicakurup8/monicakurup8.github.io/static/js/main.0d940e11.chunk.js"
+  },
+  {
+    "revision": "5e5baa012f702804d6f3",
+    "url": "/monicakurup8/monicakurup8.github.io/static/js/runtime-main.1669699a.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/monicakurup8/monicakurup8.github.io/static/media/logo.25bf045c.svg"
+  }
+]);
